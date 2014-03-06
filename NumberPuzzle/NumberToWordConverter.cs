11df9153e3c42e64b5d582d53a2688dd2670a035
unknown <alpriest@gmail.com>
2014-03-06 13:38:41 +0000

    Fixed bug with missing 'and' between weighted number groups.

--- NumberPuzzle/NumberToWordConverter.cs
+++ NumberPuzzle/NumberToWordConverter.cs
@@ -23,30 +23,55 @@ namespace NumberPuzzle
 		{
 			var numberGroups = SplitNumberIntoWeightedHundredGroups(number);
 
-			return numberGroups
-				.Aggregate(string.Empty, (s, @group) => AppendNumberDescription(@group, s))
-				.Trim();
+			var result = new List<string>();
+			for (var i = 0; i < numberGroups.Count(); i++)
+			{
+				result.Add(groupDescriber.Describe(numberGroups[i]));
+			}
+
+			if (ShouldInsertConjunctionBeforeFinalGroup(numberGroups))
+			{
+				result.Insert(result.Count - 1, numberDescriber.Conjunction);
+			}
+
+			return string.Join(" ", result.Where(x => !string.IsNullOrEmpty(x)));
 		}
 
-		private string AppendNumberDescription(WeightedHundredGroup @group, string s)
+		private bool ShouldInsertConjunctionBeforeFinalGroup(IList<WeightedHundredGroup> numberGroups)
 		{
-			var description = groupDescriber.Describe(@group);
-			return s + description + Appendage(description);
+			var firstGroups = numberGroups.Take(numberGroups.Count - 1);
+			var finalGroup = numberGroups.Skip(numberGroups.Count - 1).First();
+
+			if (GroupHasHundredsValue(finalGroup))
+				return false;
+
+			return AnyGroupHasAValue(firstGroups) && GroupHasAValue(finalGroup);
 		}
 
-		private static string Appendage(string description)
+		private static bool GroupHasAValue(WeightedHundredGroup finalGroup)
 		{
-			return string.IsNullOrEmpty(description) ? string.Empty : " ";
+			return finalGroup.HundredGroup.Number > 0;
 		}
 
-		private IEnumerable<WeightedHundredGroup> SplitNumberIntoWeightedHundredGroups(int number)
+		private static bool AnyGroupHasAValue(IEnumerable<WeightedHundredGroup> firstGroups)
+		{
+			return firstGroups.Any(x => x.HundredGroup.Number > 0);
+		}
+
+		private static bool GroupHasHundredsValue(WeightedHundredGroup finalGroup)
+		{
+			return finalGroup.HundredGroup.Hundreds > 0;
+		}
+
+		private IList<WeightedHundredGroup> SplitNumberIntoWeightedHundredGroups(int number)
 		{
 			const string format = "000,000,000";
 
 			return number
 				.ToString(format)
 				.Split(',')
-				.Select((n, i) => new WeightedHundredGroup(int.Parse(n), _groupWeightings[i]));
+				.Select((n, i) => new WeightedHundredGroup(int.Parse(n), _groupWeightings[i]))
+				.ToList();
 		}
 	}
 }
--- NumberPuzzleTests/AcceptanceTests.cs
+++ NumberPuzzleTests/AcceptanceTests.cs
@@ -13,6 +13,10 @@ namespace NumberPuzzleTests
 		[TestCase(105, "one hundred and five")]
 		[TestCase(56945781, "fifty six million nine hundred and forty five thousand seven hundred and eighty one")]
 		[TestCase(113, "one hundred and thirteen")]
+		[TestCase(1001, "one thousand and one")]
+		[TestCase(1000, "one thousand")]
+		[TestCase(10000013, "ten million and thirteen")]
+		[TestCase(10000000, "ten million")]
 		public void CanConvertExamples(int number, string expected)
 		{
 			Assert.That(ExecuteProgram(number), Is.EqualTo(expected));
